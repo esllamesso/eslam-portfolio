0f47c32fcf59c00dc174ccb0f344b60e1475eb41
Eslam Mohamed <166975147+esllamesso@users.noreply.github.com>
2025-05-11 08:27:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var typed = new Typed("#typed", {
+    strings: ["Flutter Developer", "UI Designer", "Mobile App Specialist"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+  });
+});
